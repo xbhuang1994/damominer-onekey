590b3cadf1bd969fbeb3bf9597a41914b32de47c
Henry <henry@orangegrin.com>
2022-12-06 12:12:08 +0800

    add one key

---
+++ onekey.sh
@@ -0,0 +1,68 @@
+function getJsonValuesByAwk() {
+    awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
+        foundKeyCount = 0
+        while (length(json) > 0) {
+            # pos = index(json, "\""key"\"");
+            pos = match(json, "\""key"\"[ \\t]*?:[ \\t]*");
+            if (pos == 0) {if (foundKeyCount == 0) {print defaultValue;} exit 0;}
+
+            ++foundKeyCount;
+            start = 0; stop = 0; layer = 0;
+            for (i = pos + length(key) + 1; i <= length(json); ++i) {
+                lastChar = substr(json, i - 1, 1)
+                currChar = substr(json, i, 1)
+
+                if (start <= 0) {
+                    if (lastChar == ":") {
+                        start = currChar == " " ? i + 1: i;
+                        if (currChar == "{" || currChar == "[") {
+                            layer = 1;
+                        }
+                    }
+                } else {
+                    if (currChar == "{" || currChar == "[") {
+                        ++layer;
+                    }
+                    if (currChar == "}" || currChar == "]") {
+                        --layer;
+                    }
+                    if ((currChar == "," || currChar == "}" || currChar == "]") && layer <= 0) {
+                        stop = currChar == "," ? i : i + 1 + layer;
+                        break;
+                    }
+                }
+            }
+
+            if (start <= 0 || stop <= 0 || start > length(json) || stop > length(json) || start >= stop) {
+                if (foundKeyCount == 0) {print defaultValue;} exit 0;
+            } else {
+                print substr(json, start, stop - start);
+            }
+
+            json = substr(json, stop + 1, length(json) - stop)
+        }
+    }' | sed 's/^"\(.*\)"$/\1/'
+}
+
+JSON=`curl https://api.github.com/repos/damomine/aleominer/releases/latest`
+VERSION=$(getJsonValuesByAwk "$JSON" "tag_name")
+DOWNLOAD_URL=$(getJsonValuesByAwk "$JSON" "browser_download_url")
+echo "the last version" $VERSION
+if [ ! -d /opt/damominer_$VERSION  ];then
+  mkdir /opt/damominer_$VERSION
+  curl -L $DOWNLOAD_URL -o /opt/damominer_$VERSION.tar
+  tar xvf /opt/damominer_$VERSION.tar -C /opt/damominer_$VERSION
+  chmod +x /opt/damominer_$VERSION/damominer
+  killall damominer
+else
+  echo version exist
+fi
+
+if ps aux | grep 'damominer' | grep -q 'proxy'; then
+    echo "DamoMiner already running."
+    exit 1
+else
+    echo "DamoMiner is running."
+    nohup /opt/damominer_$VERSION/damominer --address $1 --proxy asiahk.damominer.hk:9090 >> /tmp/aleo.log 2>&1 &
+    echo "Your address is $1"
+fi
